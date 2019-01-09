7cdb0d38fbfce5534dc33b5f4553817d0686a6ba
DESKTOP-3ET8JAK\maxwe <maxgomes07>
2019-01-09 15:46:17 -0200

    adaptando estrutura para gulp e yarn

---
+++ src/StoreOfBuild.Web/gulpfile.js
@@ -0,0 +1,45 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var uncss = require('gulp-uncss');
+var browserSync = require('browser-sync').create();
+
+
+gulp.task('browser-sync', function(){
+    browserSync.init({
+        proxy: 'localhost:5000'
+    })
+
+    gulp.watch('./Styles/*.css', ['css']);
+    gulp.watch('./js/*.js', ['js']);
+
+}); 
+
+gulp.task('js', function(){
+    return gulp.src([
+        './node_modules/bootstrap/dist/js/bootstrap.min.js',
+        './node_modules/jquery/dist/jquery.min.js',
+        './node_modules/jquery-validation/dist/jquery.validate.min.js',
+        './node_modules/jquery-validation-unobtrusive/dist/jquery.validate.unobtrusive.min.js',
+        './js/site.js'
+    ])
+    .pipe(gulp.dest('wwwroot/js/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('css', function(){
+    return gulp.src([
+        './node_modules/bootstrap/dist/css/bootstrap.min.css',
+        './Styles/site.css'
+    ])
+    .pipe(concat('site.min.css'))
+    .pipe(cssmin())
+    .pipe(uncss({html:['Views/**/*.cshtml']}))
+    .pipe(gulp.dest('wwwroot/css/'))
+    .pipe(browserSync.stream());
+
+});
+
+gulp.task('css-watch', function(){
+    gulp.watch('./wwwroot/styles/**/*.css', gulp.series('css'))
+})
