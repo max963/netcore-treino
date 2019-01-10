60b59da569f3c8724f9d5a5af7b7beb397d78587
DESKTOP-3ET8JAK\maxwe <maxgomes07>
2019-01-09 22:44:16 -0200

    Add context class

---
+++ src/StoreOfBuild.Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using StoreOfBuild.Domain.Products;
+
+namespace StoreOfBuild.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
